3b22ada374079e4da5da64736c6c6a3b1c5ac85c
nicoconti1901 <nico.conti.1901@gmail.com>
2023-08-10 15:42:35 -0300

    Se finalizo el registro de usuarion sin token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,29 @@
+import { pool } from "../db.js";
+import bcryt from "bcrypt";
+
 export const signin = (req, res) => res.send("Ingresando");
 
-export const signup = (req, res) => res.send("Registrando");
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcryt.hash(password, 10);
+    console.log(hashedPassword);
+
+    const result = await pool.query(
+      "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) returning *",
+      [username, email, hashedPassword]
+    );
+    console.log(result);
+    return res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code === "23505") {
+      res.status(400).json({
+        message: "El usuario ya existe",
+      });
+    }
+  }
+};
 
 export const logout = (req, res) => res.send("Cerrando sesion");
 
