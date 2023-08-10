11fb5946b775560a0dd957a2520714baf1702012
nicoconti1901 <nico.conti.1901@gmail.com>
2023-08-10 10:50:13 -0300

    se pueden obtener todas las tareas con la funcion getAllTasks

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,14 +1,9 @@
 import { pool } from "../db.js";
 
 export const getAllTasks = async (req, res, next) => {
-  try{
   const result = await pool.query("SELECT * FROM task");
-  console.log(result);
   return res.json(result.rows);
-} catch (error){
-  next(error);
-}
-}
+};
 
 export const getTask = (req, res) => res.send("Obteniendo tarea unica");
 
@@ -21,15 +16,17 @@ export const createTask = async (req, res, next) => {
       "INSERT INTO task (title, description) VALUES ($1, $2) returning *",
       [title, description]
     );
-    
+
     res.json(result.rows[0]);
   } catch (error) {
-    if(error.code === '23505'){
-    return res.send("La tarea ya existe");
+    if (error.code === "23505") {
+      return res.status(409).json({
+        message: "La tarea con ee titulo ya existe"
+      });
+    }
+    next(error);
   }
-  next(error);
 };
-}
 export const updateTask = (req, res) => res.send("Editando tareas");
 
 export const deleteTask = (req, res) => res.send("Borrando tarea");
